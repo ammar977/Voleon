48a3d0efaf40907f99b3c69fc1d75e2bdb25f3ea
Rabeez Riaz <19100165@lums.edu.pk>
2018-04-04 23:04:53 +0500

    Login card component implemented

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import logo from './logo.svg'
-import './App.css'
-import store from './store'
-import Customers from './components/Customer/customers'
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import logo from './logo.svg';
+import './App.css';
+import store from './store';
+// import Customers from './components/Customer/customers';
+import CardContainer from './components/Card/card';
 
 class App extends Component {
 
@@ -15,7 +16,10 @@ class App extends Component {
                         <img src={ logo } className="App-logo" alt="logo"/>
                         <h1 className="App-title">Voleon dummy homepage</h1>
                     </header>
-                    <Customers/>
+
+                    {/*<Customers/>*/}
+                    <CardContainer cardType="Login"/>
+                    <CardContainer cardType="Signup"/>
                 </div>
             </Provider>
         )
