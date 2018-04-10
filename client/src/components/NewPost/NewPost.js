ac0fee0ddd0df3edb6122522422d3fcc52a824bc
Tooba Mukhtar <toobamukhtar40@gmail.com>
2018-04-10 13:09:07 +0500

    basic_component_structure

--- client/src/components/NewPost/NewPost.js
+++ client/src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import logo from '../../Voleon.png';
 // import CardContainer from '../Card/card';
 import './NewPost.css';
 
@@ -7,9 +8,17 @@ class NewPost extends Component {
 
     render() {
         return (
-            <div className="newpost_container">
-                <p>newpost</p>
-            </div>
+           <div className= "newpost_container">
+                    <textarea placeholder="Enter your message here"></textarea> 
+                        <button class="btn blue lighten-1 waves-effect waves-light" type="submit">
+                        <span class="">Post</span>
+                        </button>
+                        <label for="file-input">
+                        <i class="material-icons blue-text text-lighten-3">&#xe439;</i>
+                        </label>
+                        <input accept="image/*" id="file-input" type="file" class="image-upload"></input>
+             
+           </div>
         );
     }
 }
