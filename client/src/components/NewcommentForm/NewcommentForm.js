0c71c1f1cd7e9416f84519f77c8824ee62b38785
Tooba Mukhtar <toobamukhtar40@gmail.com>
2018-04-11 04:45:32 +0500

    comment layout start

--- client/src/components/NewcommentForm/NewcommentForm.js
+++ client/src/components/NewcommentForm/NewcommentForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import profile from '../../profile.png';
 // import CardContainer from '../Card/card';
 import './NewcommentForm.css';
 
@@ -8,7 +9,12 @@ class NewcommentForm extends Component {
     render() {
         return (
             <div className="newcomment_container">
-                <p>newcomment</p>
+            	<div class="card-image">
+            		<img src={ profile} alt="profile-image"/>
+            	</div>
+            	<div className="comment-content">
+                	<textarea placeholder="Write a comment..."></textarea>
+                </div>
             </div>
         );
     }
