ae515986eca9f4b1695318621b21616b77df45ff
Tooba Mukhtar <toobamukhtar40@gmail.com>
2018-04-11 12:49:08 +0500

    basic post layout

--- client/src/components/NewPost/NewPost.js
+++ client/src/components/NewPost/NewPost.js
@@ -9,14 +9,19 @@ class NewPost extends Component {
     render() {
         return (
            <div className= "newpost_container">
+                <div
                     <textarea placeholder="Enter your message here"></textarea> 
-                        <button class="btn blue lighten-1 waves-effect waves-light" type="submit">
+                    <button class="btn blue lighten-1 waves-effect waves-light" type="submit">
                         <span class="">Post</span>
-                        </button>
-                        <label for="file-input">
+                    </button>
+                </div>
+
+                <div
+                    <label for="file-input">
                         <i class="material-icons blue-text text-lighten-3">&#xe439;</i>
-                        </label>
-                        <input accept="image/*" id="file-input" type="file" class="image-upload"></input>
+                    </label>
+                    <input accept="image/*" id="file-input" type="file" class="image-upload"></input>
+                </div>
              
            </div>
         );
