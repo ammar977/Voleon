a7f1a9ea6497e32452434aac36227ebf1e848a71
Tooba Mukhtar <toobamukhtar40@gmail.com>
2018-04-11 13:13:26 +0500

    basic post layout

--- client/src/components/NewPost/NewPost.js
+++ client/src/components/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../../Voleon.png';
+import profile from '../../profile.png';
 // import CardContainer from '../Card/card';
 import './NewPost.css';
 
@@ -9,14 +10,20 @@ class NewPost extends Component {
     render() {
         return (
            <div className= "newpost_container">
-                <div
+                <div class="card-image">
+                        <img src={ profile} alt="profile-image"/>
+                 </div>
+
+                <div className="post-content">
                     <textarea placeholder="Enter your message here"></textarea> 
+                </div>
+                <div className= "button">
                     <button class="btn blue lighten-1 waves-effect waves-light" type="submit">
                         <span class="">Post</span>
                     </button>
                 </div>
 
-                <div
+                <div className="camera-icon">
                     <label for="file-input">
                         <i class="material-icons blue-text text-lighten-3">&#xe439;</i>
                     </label>
