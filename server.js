dc7b7c9d4606b102edc47af3403f300f24b930e6
Ammar Ahmad <ammar.977@hotmail.com>
2018-04-15 05:36:06 +0500

    Server uploading/downloading file to/from DB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+// const bodyParser = require('body-parser');
+const bodyParser = require('busboy-body-parser');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const bcrypt = require('bcryptjs');
@@ -16,11 +17,24 @@ mongoose.connect(db.mongoURI)
 // Initialize app
 const app = express();
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
-// parse application/json
-app.use(bodyParser.json())
+// // // Parse JSON
+// app.use(bodyParser.json({limit: "50mb"}));
+// // // Parse url-encoded
+// app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 
+// parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+// app.use(bodyParser.json())
+
+app.use(bodyParser({ limit: '50mb' }));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // passport middleware
 require('./config/passport')(passport);
 app.use(passport.initialize());
@@ -29,6 +43,7 @@ app.use(passport.initialize());
 // Load routes
 const users = require('./routes/users');
 const posts = require('./routes/posts');
+const application = require('./routes/applications');
 
 // Home Page
 app.get('/', (req, res) => {
@@ -43,5 +58,5 @@ app.listen(port, () => console.log(`Server is running on ${port}`));
 // use routes
 app.use('/user',users);
 app.use('/post',posts);
-
+app.use('/application',application);
 
