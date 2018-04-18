0f6fe8e89e50b5f333b09f8cab503ee78a875055
Rabeez Riaz <19100165@lums.edu.pk>
2018-04-18 15:09:33 +0500

    Buttons in ElectionDashboard page work

--- client/src/components/CandidatesList/CandidatesList.js
+++ client/src/components/CandidatesList/CandidatesList.js
@@ -1,29 +1,45 @@
-import {Card, Col, Icon,Button} from 'react-materialize';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {Card, Col, Icon,Button} from 'react-materialize';
 import './CandidatesList.css';
+import {changePage} from '../../store/actions/form'
 
 
 class CandidatesList extends Component{
+        
+    constructor() {
+        super();
+        this.gotoPage.bind(this);
+    }
 
+    static propTypes = {
+        changePage: PropTypes.func.isRequired,
+    }
+
+    gotoPage(destinationPage) {
+        this.props.changePage(destinationPage);
+    }
+    
 	render(){
 		return (
 			<div>
-				<ul className = "collection">
-				    <li className = "collection-item avatar" >
-					    <div className="CandidateBlock">
-					    	<a href="#" className="CandidatesEntry">Taha Bin Amir </a>
+				<ul className="collection">
+				    <li className="collection-item avatar" >
+					    <div className="CandidateBlock" onClick={(e) => this.gotoPage('Profile')}>
+					    	<a href="#" className="CandidatesEntry">Taha Bin Amir</a>
 					    </div>
 					</li>
 					
-					<li className = "collection-item avatar" >
+					<li className="collection-item avatar" >
 					    <div className="CandidateBlock">
-					   		<a href="#" className="CandidatesEntry">Kinza Habib </a>
+					   		<a href="#" className="CandidatesEntry">Kinza Habib</a>
 					    </div>
 					</li>
 
-					<li className = "collection-item avatar" >
+					<li className="collection-item avatar" >
 					    <div className="CandidateBlock">
-					   		<a href="#" className="CandidatesEntry">Ammar Ahmad </a>
+					   		<a href="#" className="CandidatesEntry">Ammar Ahmad</a>
 					    </div>
 					</li>
 				</ul>	
@@ -32,4 +48,11 @@ class CandidatesList extends Component{
 	}
 }
 
-export default CandidatesList;
+const mapStateToProps = (state) => ({
+})
+
+const dispatchToProps = (dispatch) => ({
+    changePage: (destinationPage) => dispatch(changePage(destinationPage)),
+})
+
+export default connect(mapStateToProps,dispatchToProps)(CandidatesList);
--- client/src/components/ImportantDates/ImportantDates.js
+++ client/src/components/ImportantDates/ImportantDates.js
@@ -1,54 +1,73 @@
-import {Card, Col ,Button} from 'react-materialize';
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import {Card, Col ,Button} from 'react-materialize';
 import './ImportantDates.css';
+import {changePage} from '../../store/actions/form'
 
 
 class ImportantDates extends Component{
+        
+    constructor() {
+        super();
+        this.gotoPage.bind(this);
+    }
 
+    static propTypes = {
+        changePage: PropTypes.func.isRequired,
+    }
 
+    gotoPage(destinationPage) {
+        this.props.changePage(destinationPage);
+    }
+    
 	render(){
-
 		return (
-
 			<div >
 				<ul>
 				    <li className = "ImportantDates-container" >
-					   Application Deadline: 14/04/18
-					 </li>
+					    Application Deadline: 14/04/18
+					</li>
 
-				        <br/>
+				    <br/>
 
-					 <li className = "ImportantDates-container" >
-					   Polling Date: 20/04/18
-					 </li>
+					<li className = "ImportantDates-container" >
+					    Polling Date: 20/04/18
+					</li>
 
 						<br/>
-					 <li className = "ImportantDates-container" >
-					   Polling Time: 9AM to 5PM
-					 </li>
+					<li className = "ImportantDates-container" >
+					    Polling Time: 9AM to 5PM
+					</li>
 
-					 <br/>
-					 <li className = "ImportantDates-container" >
-					   Results Day: 22/04/18
-					 </li>
+					<br/>
+					<li className = "ImportantDates-container" >
+					    Results Day: 22/04/18
+					</li>
 
-					 <br/>
-					 <li className = "ImportantDates-container" >
-					   Oath Taking 23/04/18
-					 </li>
-
+					<br/>
+					<li className = "ImportantDates-container" >
+					    Oath Taking 23/04/18
+					</li>
 
 			    </ul>
 
-			     <div className='form__submit-btn-wrapper'>
-                    <Button className='blue lighten-1' waves='light' >Vote Now</Button>
-                 </div>
+			    <div className='form__submit-btn-wrapper'>
+                    <Button className='blue lighten-1' waves='light' onClick={(e) => this.gotoPage('ElectionVoting')}>Vote Now</Button>
+                </div>
+                <div className='form__submit-btn-wrapper'>
+                    <Button className='blue lighten-1' waves='light' onClick={(e) => this.gotoPage('ElectionResults')}>Results</Button>
+                </div>
              </div>
-
 			)
-
 		}
+}
 
-	}
+const mapStateToProps = (state) => ({
+})
 
-	export default ImportantDates;
+const dispatchToProps = (dispatch) => ({
+    changePage: (destinationPage) => dispatch(changePage(destinationPage)),
+})
+
+export default connect(mapStateToProps,dispatchToProps)(ImportantDates);
