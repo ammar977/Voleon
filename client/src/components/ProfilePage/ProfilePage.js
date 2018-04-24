13896d7e583433b7f280684c7fb26eab44695248
Tooba Mukhtar <toobamukhtar40@gmail.com>
2018-04-25 03:54:06 +0500

    profile page edit and posts map

--- client/src/components/ProfilePage/ProfilePage.js
+++ client/src/components/ProfilePage/ProfilePage.js
@@ -4,6 +4,10 @@ import { connect } from 'react-redux';
 import Navbar from '../Navbar/Navbar';
 import CardContainer from '../Card/card';
 import './ProfilePage.css';
+import {CardPanel, Col} from 'react-materialize';
+import cover from '../../cover.jpg';
+import profile from '../../profile.png';
+import picture from '../../picture.jpg';
 
 
 class ProfilePage extends Component {
@@ -14,12 +18,32 @@ class ProfilePage extends Component {
 
     render() {
         console.log('in profile page', this.props.logged.passedArgs);
+
+        const user_post = this.props.logged.passedArgs.posts.map(p => 
+            <CardContainer cardType="New Post" post={p} key={p._id}/>
+        );
         return (
             <div className='profilePage_container'>
 				<Navbar/>
 				
 				<div className="page_contents">
-                    <p>this is the profile of {this.props.logged.passedArgs.user.firstName}</p>
+                    <figure>
+                        <img src={picture}  id="profile-img"/>
+                        <figcaption> <b>{this.props.logged.passedArgs.user.firstName} {this.props.logged.passedArgs.user.lastName}</b></figcaption>
+                    </figure>
+                    <div className="row">
+                        <div className="col s12 m4">
+                            <div className="indigo lighten-3 black-text">
+                                <div className="card-content">
+                                    <h5 className="centre"><b>Manifesto</b></h5>
+                                    <p className="manifesto">As the voice of the students,my vision is to bring to light the universities greatest resource,the power of the students and their well being,specifically supporting more social activities.</p>
+                                </div>  
+                            </div>
+                        </div>
+                    </div>   
+                    <div className="user-post">
+                        {user_post}
+                    </div> 
 
 	                <div id="dummy"></div>
                 </div>
