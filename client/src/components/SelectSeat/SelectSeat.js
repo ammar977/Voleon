244f5b1356b0103f21b7ed625cb05f8425bb09b0
Tooba Mukhtar <toobamukhtar40@gmail.com>
2018-05-09 15:33:01 +0500

    select seat

--- client/src/components/SelectSeat/SelectSeat.js
+++ client/src/components/SelectSeat/SelectSeat.js
@@ -5,6 +5,7 @@ import {Card, Col, Icon,Button} from 'react-materialize';
 import './SelectSeat.css';
 import {changePage} from '../../store/actions/form'
 
+
 class SelectSeat extends Component{
     
     constructor() {
@@ -13,42 +14,37 @@ class SelectSeat extends Component{
     }
 
     static propTypes = {
-		changePage: PropTypes.func.isRequired,
-		logged: PropTypes.object,
+        changePage: PropTypes.func.isRequired,
+        logged: PropTypes.object,
     }
 
     gotoPage(destinationPage, seatObj) {
-    	console.log('here', destinationPage, seatObj);
+        console.log('here', destinationPage, seatObj);
         this.props.changePage(destinationPage, seatObj);
     }
-    
-	render(){
-		console.log('in select seat', this.props);
-		return (
-			<div>
-				<ul className="collection">
-
-					{
-						this.props.logged.electionSeats.map(seat => {
-							return <li className="collection-item avatar" onClick={(e) => this.gotoPage('ElectionDashboard', seat)} key={seat._id}>
-							    <p>
-							     <img src="sample.jpg" alt=" " className="circle" hspace='10' />
-									{seat.electionId}
-									<a href="#" className="secondary-content">
-										<i className="material-icons"> forward </i>
-									</a>
-								</p>
-							</li>
-						})
-					}
-				 </ul>
-			</div>
-			)
-	}
+  
+    render(){
+        console.log('in select seat', this.props);
+        return (
+            <div className='Selectseat_container'>
+                <ul className="collection">
+                {
+                    this.props.logged.electionSeats.map(seat => {
+                        return <li className="collection-item avatar" onClick={(e) => this.gotoPage('ElectionDashboard', seat)} key={seat._id}>
+                               <i class="material-icons circle">chevron_right</i>
+                               <span class="title">{seat.electionId}</span>
+                               </li>
+                        })
+                }
+                </ul>
+            
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state) => ({
-	logged: state.logged,
+    logged: state.logged,
 })
 
 const dispatchToProps = (dispatch) => ({
