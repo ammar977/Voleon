9b4338b94a80c6888317496e7d721beacc751da1
Ammar Ahmad <ammar.977@hotmail.com>
2018-05-13 22:48:50 +0500

    ready for deployment

--- server.js
+++ server.js
@@ -83,7 +83,7 @@ app.get('/', (req, res) => {
     res.send(message);
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on ${port}`));
 
 // use routes
