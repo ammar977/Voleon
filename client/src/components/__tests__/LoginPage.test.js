9ca2f408321dfd81c616b4d96be1967af6aadd43
Tooba Mukhtar <toobamukhtar40@gmail.com>
2018-05-15 19:44:46 +0500

    setup done huraaay

--- client/src/components/__tests__/LoginPage.test.js
+++ client/src/components/__tests__/LoginPage.test.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { shallow, mount, render } from 'enzyme';
 import LoginPage from '../LoginPage/LoginPage';
 
-// describe what we are testing
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+configure({ adapter: new Adapter() });
+
 describe('Login Component', () => {
  
  // make our assertion and what we expect to happen 
  it('should render without throwing an error', () => {
-   expect(shallow(<Login />).exists(<form className='login'></form>)).toBe(true)
+   expect(shallow(<LoginPage />).exists(<form className='login'></form>)).toBe(true)
  })
 })
--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -1,6 +1,5 @@
-const Enzyme = require('enzyme');
-// this is where we reference the adapter package we installed  
-// earlier
-const EnzymeAdapter = require('enzyme-adapter-react-15');
-// This sets up the adapter to be used by Enzyme
-Enzyme.configure({ adapter: new EnzymeAdapter() });
+// setup file
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
